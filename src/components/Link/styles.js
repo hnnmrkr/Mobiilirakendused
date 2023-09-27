77eabf2114a571b9f371a9ab3e75bf3dd2b1b116
hnnmrkr <hanna-marii.kriisa@voco.ee>
2023-09-27 12:19:31 +0300

    Created color management file

--- src/components/Link/styles.js
+++ src/components/Link/styles.js
@@ -1,11 +1,12 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../utils/colors";
 
 export const styles = StyleSheet.create({
     container: {
         marginTop: 30
     },
     title: {
-        color: '#4F63AC',
+        color: colors.blue,
         textAlign: 'center',
         fontSize: 16,
         fontWeight: 'bold'
